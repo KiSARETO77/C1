7c4c12ac6bdd5188e0fab5d1f649267d5c793c59
KiSARETO77 <106799027+KiSARETO77@users.noreply.github.com>
2022-07-18 15:40:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 import aminolib,ujson,time
 for a in ujson.load(open("accounts.json")):
-	e,p,d,SID=a["email"],a["password"],a["device"],a["SID"]
-	c = aminolib.Client(d)
-	try:
+		e,p,d,SID=a["email"],a["password"],a["device"],a["SID"]
+		c = aminolib.Client(d)
 		c.login(e,p)
 		print(f'Logged in {e}')
 		c.join_community("105025706")
-		c.check_lottery("105025706")
-	except Exception as e:
-		print(e)
-		pass
 		for i in range(24):
 			try:
 				c.send_active("105025706")
