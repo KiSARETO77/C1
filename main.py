5316fb5525f2458efe15c45665b220f2a4b06620
KiSARETO77 <106799027+KiSARETO77@users.noreply.github.com>
2022-07-18 16:08:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-import aminolib,ujson,time
-for a in ujson.load(open("accounts.json")):
-		e,p,d,SID=a["email"],a["password"],a["device"],a["SID"]
-		c = aminolib.Client(d)
-		c.login(e,p)
-		print(f'Logged in {e}')
-		c.join_community("105025706")
-		for i in range(24):
-			try:
-				c.send_active("105025706")
-				print(f"send time {i+1}")
-				time.sleep(12)
-			except Exception as e:
-				time.sleep(20)
-				print(e)
-				pass
+from os import system as V
+while True:
+	V("python C.py")
